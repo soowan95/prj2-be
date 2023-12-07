07337a73c117afa7029082c5d79d000ca197f401
1082cubi <1082cubi@hanmail.net>
2023-12-07 17:49:37 +0900

    chartpage별 담은 노래 보여주기,playlist별 담은 노래갯수 보여주기

--- src/main/java/com/example/prj2be/service/PlaylistService.java
+++ src/main/java/com/example/prj2be/service/PlaylistService.java
@@ -37,6 +37,7 @@ public class PlaylistService {
             list.setIsLike(likeMapper.isLike(list.getId(), list.getListId()) == 1);
             //첫페이지에서 내가 좋아요 누른 거를 볼 수 있게 list의 id랑 list의 listId가 값이 1이면 ture 0이면 false
             list.setTotalSongCount(mapper.chartlist(Integer.parseInt(list.getListId())).size());
+            //setTotalSongCount은 domain TotalSongCount에 저장할건데 chartlist의 ListId를 불러와서 갯수를 카운트하고 싶은데 String이라서 Integer로 형변환해서 카운트
         }
         return playList;
     }
